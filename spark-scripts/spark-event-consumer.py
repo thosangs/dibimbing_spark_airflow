a6b068bf5956b97d3871f71662bf06f97c733d95
Seceng <thosan.girisona@gmail.com>
2024-09-01 10:27:04 +0700

    fix: add checkpoint access to spark container

--- spark-scripts/spark-event-consumer.py
+++ spark-scripts/spark-event-consumer.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
+from pyspark.sql.functions import from_json, col, avg
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType
 
 dotenv_path = Path("/opt/app/.env")
 load_dotenv(dotenv_path=dotenv_path)
@@ -14,15 +16,31 @@ kafka_topic = os.getenv("KAFKA_TOPIC_NAME")
 
 spark_host = f"spark://{spark_hostname}:{spark_port}"
 
-os.environ[
-    "PYSPARK_SUBMIT_ARGS"
-] = "--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2 org.postgresql:postgresql:42.2.18"
-
-sparkcontext = pyspark.SparkContext.getOrCreate(
-    conf=(pyspark.SparkConf().setAppName("DibimbingStreaming").setMaster(spark_host))
+os.environ["PYSPARK_SUBMIT_ARGS"] = (
+    "--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.2 org.postgresql:postgresql:42.2.18"
 )
-sparkcontext.setLogLevel("WARN")
-spark = pyspark.sql.SparkSession(sparkcontext.getOrCreate())
+
+spark = (
+    pyspark.sql.SparkSession.builder.appName("DibimbingStreaming")
+    .master(spark_host)
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0")
+    .config("spark.sql.shuffle.partitions", 4)
+    .config("spark.sql.streaming.forceDeleteTempCheckpointLocation", True)
+    .getOrCreate()
+)
+
+spark.sparkContext.setLogLevel("WARN")
+
+schema = StructType(
+    [
+        StructField("order_id", StringType(), True),
+        StructField("customer_id", IntegerType(), True),
+        StructField("furniture", StringType(), True),
+        StructField("color", StringType(), True),
+        StructField("price", IntegerType(), True),
+        StructField("ts", LongType(), True),
+    ]
+)
 
 stream_df = (
     spark.readStream.format("kafka")
@@ -32,10 +50,11 @@ stream_df = (
     .load()
 )
 
-(
-    stream_df.selectExpr("CAST(value AS STRING)")
-    .writeStream.format("console")
-    .outputMode("append")
-    .start()
-    .awaitTermination()
-)
+parsed_df = stream_df.selectExpr("CAST(value AS STRING)").select(from_json(col("value"), schema).alias("data")).select("data.*")
+
+avg_price_df = parsed_df.groupBy("furniture").agg(avg("price").alias("avg_price"))
+
+# Write the result to the console
+query = avg_price_df.writeStream.outputMode("complete").format("console").trigger(processingTime="10 seconds").start()
+
+query.awaitTermination()
